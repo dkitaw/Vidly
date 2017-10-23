a14c0fd8e0e39c4b22158e173458922c3f03100e
nagendrababuus <nagendrababuus@gmail.com>
2017-10-22 18:00:13 -0700

    after 4 th

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -28,6 +28,52 @@ namespace Vidly.Controllers
             var movies = _context.Movies.Include(g=>g.Genre).ToList();
             return View(movies);
         }
+        public ActionResult New()
+        {
+            
+            var genre = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel()
+            {
+                Genre = genre
+            };
+            return View("MovieForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id==0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var MovieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                MovieInDb.Id = movie.Id;
+                MovieInDb.Name = movie.Name;
+                MovieInDb.ReleaseDate = movie.ReleaseDate;
+                MovieInDb.DateAdded = DateTime.Now;
+                MovieInDb.GenreId = movie.GenreId;
+                MovieInDb.NumberInStock = movie.NumberInStock;
+            }
+            
+            _context.SaveChanges();
+            return RedirectToAction("Index","Movies");
+        }
+        public ActionResult Edit(int id)
+        {
+            
+            var movie = _context.Movies.Single(m=>m.Id==id);
+            if (movie == null)
+                return HttpNotFound();  
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genre=_context.Genres.ToList()
+            };
+
+            return View("MovieForm",viewModel);
+        }
         [Route("/Movies/Details/{Id}")]
         public ActionResult Details(int Id)
         {
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,14 +9,23 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(255)]
         public string Name { get; set; }
+
+        [Display(Name ="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
-        [Required]
+        
         public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
+
+        [Display(Name = "Numbre In Stock")]
         public byte NumberInStock { get; set; }
 
     }
